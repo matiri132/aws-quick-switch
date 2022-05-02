350d7164562a87fcdd0d8c2262a514e2e82a93db
matiri132 <matias@craftech.io>
2022-05-02 02:22:13 -0300

    chore: fix functionality of verbose

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # AWS QUICK SWITCH
 Script for quick aws profile switching
 
+### Configure (Optional)
+
+You can change some values in the script as:
+* AWS_CRED: Where the AWS credentials file is located. ~/.aws/credentials as default.
+* AWS_CONF: Where the AWS confgi file is located. ~/.aws/config as default.
+* VERBOSE: If you set this to 1, you can get the list of profiles with this source profile associated.
+
 ### Install (For zsh )
 
 ```
@@ -14,7 +21,6 @@ Note: if you're not using zsh set the SOURCE variable in install.sh to your prof
 
 ### Use
 
-- `aws_sp` will list all your aws configured profiles.
-- `aws_sp -v` will list all your aws configured profiles with this source profile if its a assumed one.
+- `aws_sp` will list all your aws configured profiles with this source profile if its a assumed one.
 - `aws_sp n` Will set the selected profile, where "n" is the profile NUMBER obtained by `aws_sp`.
 
--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+BIGBLUE='\033[1;34m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
 
 INSTALL_DIR="/home/$USER/.local/bin"
 SCRIPT="aws_quick_switch.sh"
 SOURCE="/home/$USER/.zshrc"
 
-
-if [ $1 == "update" ]; then
+if [ -f "$INSTALL_DIR/aws_quick_switch.sh" ]; then
     cp "$SCRIPT" "$INSTALL_DIR"/aws_quick_switch.sh
     chmod +x "$INSTALL_DIR"/"$SCRIPT"
+    printf "${GREEN}AWS Quick Switch Updated successfully...${NC}\n"
     exit 0
 else
     cp "$SCRIPT" "$INSTALL_DIR"/aws_quick_switch.sh
     chmod +x "$INSTALL_DIR"/"$SCRIPT"
-    echo "alias aws_sp='function a(){if [ ! \"\$1\" = \"\" ];then \$(aws_quick_switch.sh \"\$1\"); else; aws_quick_switch.sh; fi;  };a'" >> ${SOURCE}
+    printf "${GREEN}AWS Quick Switch Installed successfully...\n"
+    echo "alias aws_sp='function a(){if [ ! -z \$1 ];then \$(aws_quick_switch.sh \$1); else; aws_quick_switch.sh; fi;  };a'" >> ${SOURCE}
+    printf "${GREEN}Added ALIAS for aws_sp in ${YELLOW}$SOURCE${YELLOW}...${NC}\n"
 fi
+
 
